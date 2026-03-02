0cbae772ea556fe6d3167106424c4243391b0b06
JWilkins028 <7763071+JWilkins028@users.noreply.github.com>
2026-03-01 21:21:04 -0500

    Update script.js
    - New functions to utilize stat line templates
    - Support new transformed data objects

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const [profileNavItemTemplate, profileTabPaneTemplate, playlistStatGroupTemplate, lifetimeStatGroupTemplate] = document.querySelectorAll("template");
+const profileNav = document.getElementById("profileNav");
+const profileTabContent = document.getElementById("profileTabContent");
+
+const lifetimeDataMap = {
+    "Season Reward Level": "SRLUrl",
+    "Wins": "wins",
+    "MVPs": "mvps",
+    "Goals": "goals",
+    "Shots": "shots",
+    "Goal Shot Ratio": "goalShotRatio",
+    "Assists": "assists",
+    "Saves": "saves"
+};
+
 const switchTab = false;
 
 const showView = (tab) => {
@@ -5,71 +20,25 @@ const showView = (tab) => {
     document.querySelectorAll(`.viewNavContent .${tab}`).forEach(tabPane => tabPane.classList.add("show", "active"));
 }
 
-const updatePlaylistView = (player, playlistName, playlistData) => {
-    if (playlistData) {
-        const tier = playlistData.stats.tier.value % 3 ? playlistData.stats.tier.value % 3 : 3;
-        document.querySelector(`#${player}-tab-pane .${playlistName}RatingIcon`).src = playlistData.stats.rating.metadata.iconUrl;
-        document.querySelector(`#${player}-tab-pane .${playlistName}TierDiv`).innerText = tier + "-" + (playlistData.stats.division.value + 1);
-        document.querySelector(`#${player}-tab-pane .${playlistName}MMR`).innerText = playlistData.stats.rating.value;
-    } else {
-        document.querySelector(`#${player}-tab-pane .${playlistName}MMR`).innerHTML = "&mdash;";
-    }
+const toggleTheme = () => {
+    const newTheme = document.documentElement.dataset.bsTheme === "light" ? "dark" : "light";
+    const themeIcon = document.querySelector("#themeToggle i");
+    const themeText = document.querySelector("#themeToggle span");
+
+    document.documentElement.dataset.bsTheme = newTheme;
+    themeIcon.classList.toggle("fa-sun");
+    themeIcon.classList.toggle("fa-moon");
+    themeText.innerText = newTheme;
 }
 
-const updatePlayerStats = (player, playerData) => {
-    const playlists = playerData.data.segments.filter((seg) => seg.type == "playlist");
-    const peakRatings = playerData.data.segments.filter((seg) => seg.type == "peak-rating");
-    const overview = playerData.data.segments.filter((seg) => seg.type == "overview")[0];
-
-    // Load current playlist ratings
-    document.querySelector(`#${player}-tab-pane .lastUpdated`).innerText = new Date(playerData.data.metadata.lastUpdated.value).toLocaleString([], { dateStyle: 'short', timeStyle: 'short' });
-    updatePlaylistView(player, "ThreesCurr", playlists.filter((seg) => seg.metadata.name == "Ranked Standard 3v3")[0]);
-    updatePlaylistView(player, "TwosCurr", playlists.filter((seg) => seg.metadata.name == "Ranked Doubles 2v2")[0]);
-    updatePlaylistView(player, "OnesCurr", playlists.filter((seg) => seg.metadata.name == "Ranked Duel 1v1")[0]);
-    updatePlaylistView(player, "RumbleCurr", playlists.filter((seg) => seg.metadata.name == "Rumble")[0]);
-    updatePlaylistView(player, "HoopsCurr", playlists.filter((seg) => seg.metadata.name == "Hoops")[0]);
-    updatePlaylistView(player, "DropshotCurr", playlists.filter((seg) => seg.metadata.name == "Dropshot")[0]);
-    updatePlaylistView(player, "SnowdayCurr", playlists.filter((seg) => seg.metadata.name == "Snowday")[0]);
-    document.querySelector(`#${player}-tab-pane .CasualCurrMMR`).innerText = playlists.filter((seg) => seg.metadata.name == "Casual")[0].stats.rating.value;
-
-    // Load lifetime stats
-    document.querySelector(`#${player}-tab-pane .SRL`).src = overview.stats.seasonRewardLevel.metadata.iconUrl;
-    document.querySelector(`#${player}-tab-pane .Wins`).innerText = overview.stats.wins.value;
-    document.querySelector(`#${player}-tab-pane .MVPs`).innerText = overview.stats.mVPs.value;
-    document.querySelector(`#${player}-tab-pane .Goals`).innerText = overview.stats.goals.value;
-    document.querySelector(`#${player}-tab-pane .Shots`).innerText = overview.stats.shots.value;
-    document.querySelector(`#${player}-tab-pane .GSR`).innerText = overview.stats.goalShotRatio.displayValue + "%";
-    document.querySelector(`#${player}-tab-pane .Assists`).innerText = overview.stats.assists.value;
-    document.querySelector(`#${player}-tab-pane .Saves`).innerText = overview.stats.saves.value;
-}
-
-const updatePlayerHistory = (player, historyData) => {
-
-}
-
+// On page load
 window.onload = () => {
     const urlParams = new URLSearchParams(window.location.search);
 
-    const profileNavItemTemplate = document.getElementById("profileNavItemTemplate");
-    const profileTabPaneTemplate = document.getElementById("profileTabPaneTemplate");
-    const profileNav = document.getElementById("profileNav");
-    const profileTabContent = document.getElementById("profileTabContent");
-
     // Load player data
-    for (const [player, playerData] of urlParams) {
-        dataObj = JSON.parse(playerData);
-        const newNavItem = document.importNode(profileNavItemTemplate.content, true);
-        const navButton = newNavItem.getElementById("template-tab");
-        navButton.id = `${player}-tab`;
-        navButton.dataset.bsTarget = `#${player}-tab-pane`;
-        navButton.innerText = dataObj.data.platformInfo.platformUserHandle;
-        profileNav.appendChild(newNavItem);
-
-        const newTabPane = document.importNode(profileTabPaneTemplate.content, true);
-        newTabPane.getElementById("template-tab-pane").id = `${player}-tab-pane`;
-        profileTabContent.appendChild(newTabPane);
-
-        updatePlayerStats(player, dataObj);
+    for (const [player, data] of urlParams) {
+        playerData = JSON.parse(data);
+        loadPlayerData(player, playerData);
     }
 
     if (switchTab) {
@@ -86,7 +55,72 @@ window.onload = () => {
         const searchValue = searchInput.value;
         bootstrap.Modal.getInstance(document.getElementById("searchModal")).hide();
         searchInput.value = "";
-        // console.log(`shortcuts://run-shortcut?name=RL%20Tracker&input=text&text=${encodeURIComponent(searchValue)}`);
         window.open(`shortcuts://run-shortcut?name=RL%20Tracker&input=text&text=${encodeURIComponent(searchValue)}`);
     });
+}
+
+const loadPlayerData = (player, playerData) => {;
+    profileNav.appendChild(createNavItem(player, playerData.username));
+    profileTabContent.appendChild(createProfileTabPane(player, playerData.lastUpdated, playerData.playlists, playerData.lifetime));
+}
+
+const createNavItem = (player, username) => {
+    const newNavItem = document.importNode(profileNavItemTemplate.content, true);
+
+    const navButton = newNavItem.getElementById("template-tab");
+    navButton.id = `${player}-tab`;
+    navButton.dataset.bsTarget = `#${player}-tab-pane`;
+    navButton.innerText = username;
+
+    return newNavItem;
+}
+
+const createProfileTabPane = (player, lastUpdated, allPlaylistData, lifetimeData) => {
+    const newTabPane = document.importNode(profileTabPaneTemplate.content, true);
+    const playlistStats = newTabPane.getElementById("playlistStats");
+    const lifetimeStats = newTabPane.getElementById("lifetimeStats");
+    newTabPane.getElementById("template-tab-pane").id = `${player}-tab-pane`;
+    newTabPane.querySelector('.lastUpdated').innerText = lastUpdated;
+
+    // Loop through playlists
+    for (const playlistData of allPlaylistData) {
+        if (playlistData.name !== "Casual") {
+            playlistStats.append(createPlaylistStatGroup(playlistData));
+        } else {
+            playlistStats.append(createLifetimeStatGroup(playlistData.name, playlistData.currentMMR));
+        }
+    }
+
+    // Loop through lifetime stats
+    for (const [statName, statKeyName] of Object.entries(lifetimeDataMap)) {
+        lifetimeStats.append(createLifetimeStatGroup(statName, lifetimeData[statKeyName]));
+    }
+
+    return newTabPane;
+}
+
+const createPlaylistStatGroup = (playlistData) => {
+    const newPlaylistStatGroup = document.importNode(playlistStatGroupTemplate.content, true);
+
+    newPlaylistStatGroup.querySelector(".playlistName").innerText = playlistData.name;
+    newPlaylistStatGroup.querySelector(".ratingIcon").src = "https://trackercdn.com/cdn/tracker.gg/rocket-league/ranks/" + playlistData.currentRankImg;
+    newPlaylistStatGroup.querySelector(".tierDiv").innerText = playlistData.currentRank + "-" + playlistData.currentDiv;
+    newPlaylistStatGroup.querySelector(".mmrValue").innerText = playlistData.currentMMR;
+
+    return newPlaylistStatGroup;
+}
+
+const createLifetimeStatGroup = (statName, statValue) => {
+    const newLifetimeStatGroup = document.importNode(lifetimeStatGroupTemplate.content, true);
+
+    newLifetimeStatGroup.querySelector(".statName").innerText = statName;
+    if (statName === "Season Reward Level") {
+        newLifetimeStatGroup.querySelector(".statIcon").src = "https://trackercdn.com/cdn/tracker.gg/rocket-league/ranks/" + statValue;
+        newLifetimeStatGroup.querySelector(".statValue").remove();
+    } else {
+        newLifetimeStatGroup.querySelector(".statValue").innerText = statValue;
+        newLifetimeStatGroup.querySelector(".statIcon").remove();
+    }
+
+    return newLifetimeStatGroup;
 }
